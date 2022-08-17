d2d83468ac1e096dd2e693813946f7b1d38576c3
arunanbu1001 <arunanbu10012001@gmail.com>
2022-08-17 09:54:52 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import Head from './header/head.js';
+function App() {
+  return (
+        <Head />
+  );
+}
+
+export default App; 
---
+++ src/header/head.js
@@ -0,0 +1,11 @@
+
+import React from "react"
+import "./head.css"
+
+function Head() {
+  return (
+    <div>head</div>
+  )
+}
+
+export default Head
